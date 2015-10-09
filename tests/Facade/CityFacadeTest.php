0310140f9fa4a6b1a3b373f84d24ad272139f568
Felipe Pieretti Umpierre <umpierre.felipe@gmail.com>
2015-10-09 02:46:08 +0000

    Added tests to City

--- tests/Facade/CityFacadeTest.php
+++ tests/Facade/CityFacadeTest.php
@@ -1,21 +1,78 @@
 <?php
 
 use Entity\City;
+use Entity\Hotel;
 use Facade\CityFacade;
+use Facade\HotelFacade;
 
 class CityFacadeTest extends PHPUnit_Framework_TestCase
 {
-    public function testAdd()
+    public function testCity()
     {
-        $city = new City(1, "Brasil");
+        $city = new City(15475, 'Düsseldorf');
 
         $cityFacade = new CityFacade();
         $cityFacade->add($city);
 
-        $testCity = $cityFacade->get(1);
+        return $cityFacade;
+    }
 
-        $this->assertNotNull($testCity);
-        $this->assertEquals(1, $testCity->getId());
-        $this->assertEquals("Brasil", $testCity->getName());
+    /**
+     * @depends testCity
+     */
+    public function testAdd(CityFacade $cityFacade)
+    {
+        $city = $cityFacade->get(15475);
+
+        $this->assertInstanceOf('Entity\City', $city);
+        $this->assertEquals(15475, $city->getId());
+        $this->assertEquals('Düsseldorf', $city->getName());
+    }
+
+    /**
+     * @depends testCity
+     */
+    public function testUpdate(CityFacade $cityFacade)
+    {
+        $city = $cityFacade->get(15475);
+        $city->setName('Airbnb');
+
+        $cityFacade->update($city);
+
+        $updatedCity = $cityFacade->get(15475);
+
+        $this->assertEquals('Airbnb', $updatedCity->getName());
+    }
+
+    /**
+     * @depends testCity
+     */
+    public function testAddHotel(CityFacade $cityFacade)
+    {
+        $city = $cityFacade->get(15475);
+
+        $hotel = new Hotel(1, 'Hilton Düsseldorf', 'Georg-Glock-Straße 20, 40474 Düsseldorf');
+
+        $hotelFacade = new HotelFacade();
+        $hotelFacade->add($hotel);
+
+        $cityFacade->addHotel($city, $hotelFacade);
+
+        $city = $cityFacade->get(15475);
+
+        $this->assertInternalType('array', $city->getHotel());
+        $this->assertInstanceOf('Facade\HotelFacade', $city->getHotel()[0]);
+    }
+
+    /**
+     * @depends testCity
+     */
+    public function testDelete(CityFacade $cityFacade)
+    {
+        $cityFacade->delete(15475);
+
+        $city = $cityFacade->get(15475);
+
+        $this->assertNotInstanceOf('Entity\City', $city);
     }
 }
