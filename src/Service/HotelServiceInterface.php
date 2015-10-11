df03043e5804fbb7aba6490dd07ef1662ba64c83
Felipe Pieretti Umpierre <umpierre.felipe@gmail.com>
2015-10-11 21:24:29 +0000

    Change protocols

--- src/Service/HotelServiceInterface.php
+++ src/Service/HotelServiceInterface.php
@@ -8,15 +8,7 @@ interface HotelServiceInterface
      * Will return a list of hotels by partner name
      *
      * @param $partnerName
-     * @return \Entity\Hotel[]
+     * @return array of Hotel
      */
     public function getHotelFromPartnerName($partnerName);
-
-    /**
-     * Will return a list of hotels by price
-     *
-     * @param $price
-     * @return \Entity\Hotel[]
-     */
-    public function getHotelByPrice($price);
 }
