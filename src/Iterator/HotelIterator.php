b2cd452cfb0d8c8ae59622e8ada1246f5cca3ca7
Felipe Pieretti Umpierre <umpierre.felipe@gmail.com>
2015-11-23 23:58:17 +0000

    Iterator for all Entities

---
+++ src/Iterator/HotelIterator.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Iterator;
+
+use Facade\HotelFacade;
+
+class HotelIterator implements \Iterator
+{
+    private $facade;
+    private $currentIndex = 0;
+
+    public function __construct(HotelFacade $facade)
+    {
+        $this->facade = $facade;
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Return the current element
+     * @link http://php.net/manual/en/iterator.current.php
+     * @return mixed Can return any type.
+     */
+    public function current()
+    {
+        return $this->facade->get($this->currentIndex);
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Move forward to next element
+     * @link http://php.net/manual/en/iterator.next.php
+     * @return void Any returned value is ignored.
+     */
+    public function next()
+    {
+        $this->currentIndex++;
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Return the key of the current element
+     * @link http://php.net/manual/en/iterator.key.php
+     * @return mixed scalar on success, or null on failure.
+     */
+    public function key()
+    {
+        return $this->currentIndex;
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Checks if current position is valid
+     * @link http://php.net/manual/en/iterator.valid.php
+     * @return boolean The return value will be casted to boolean and then evaluated.
+     * Returns true on success or false on failure.
+     */
+    public function valid()
+    {
+        return null !== $this->facade->get($this->currentIndex);
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Rewind the Iterator to the first element
+     * @link http://php.net/manual/en/iterator.rewind.php
+     * @return void Any returned value is ignored.
+     */
+    public function rewind()
+    {
+        $this->currentIndex = 0;
+    }
+}
